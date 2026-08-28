6750ed66f32194affca2f01415e13476bec40963
newindia <amaltms@gmail.com>
2026-08-28 20:16:44 +0530

    Fix employee default password length for Supabase Auth

--- constants.ts
+++ constants.ts
@@ -1,5 +1,3 @@
-
-export const DEFAULT_EMPLOYEE_PASSWORD = '12345';
+export const DEFAULT_EMPLOYEE_PASSWORD = '123456';
 export const DEFAULT_ADMIN_PASSWORD = '654321';
 export const ADMIN_USER_ID = 'admin';
-   
